7c2c32d1fa5ca79b62b4c9153a533f39f58640d7
dylanbram354 <dylanbram354@gmail.com>
2021-06-11 13:27:59 -0500

    Finished MVP

--- music_library_api/music/views.py
+++ music_library_api/music/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework.views import APIView
@@ -21,7 +22,30 @@ class SongList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
 class SongDetail(APIView):
 
-    def get_by_id(self, request):
-        song = 
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(id=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        song.delete()
+        return Response(serializer.data, status=status.HTTP_200_OK)
